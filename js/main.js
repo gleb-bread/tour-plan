1549e4fa583806a6f5d03536c890fd086f256b5a
gleb-bread <glebhets123@gmail.com>
2022-01-27 02:31:30 +0200

    create: reviews

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-const swiper = new Swiper('.swiper', {
+const hotelSwiper = new Swiper('.hotel-swiper', {
     loop: true,
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button--next',
-        prevEl: '.swiper-button--prev',
+        nextEl: '.hotel-swiper__button--next',
+        prevEl: '.hotel-swiper__button--prev',
     },
     keyboard: {
         enabled: true,
         onlyInViewport: false,
     },
 });
+const reviewSwiper = new Swiper('.reviews-slider', {
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.reviews-slider__button--next',
+        prevEl: '.reviews-slider__button--prev',
+    }
+});
+
 document.addEventListener('click', function Click(e) {
     console.log(e.target);
 });
